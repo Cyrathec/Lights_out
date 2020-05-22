e459e7b261c85e4537b6e16f6812ef34b9918a88
Cyrathec <loic.did@gmail.com>
2020-05-22 15:26:49 +0200

    images sdl

--- SDL_local.c
+++ SDL_local.c
@@ -1,42 +1,81 @@
 #include "SDL_local.h"
 
-/*
 void Affichage_jeu(){
+
+	// Instanciation des pointeurs de la SDL
     SDL_Window* window;
     SDL_Renderer* renderer;
 
     SDL_Event e;
 
-    int quit = 0;
+	// Initialisation des variables
+    int quit = 0; // variable de test pour quitter le programme
+	int windowh; // hauteur de la fenêtre de la SDL
+	int windoww; // largeur de la fenêtre de la SDL
 
-	if (!SDL_WasInit(SDL_INIT_VIDEO))
-	{
-		if (SDL_Init(SDL_INIT_VIDEO) != 0)
-		{
+	// Initialisation de la sdl
+	if (!SDL_WasInit(SDL_INIT_VIDEO)) {
+		if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 			printf("[-] ERROR - Failed to initialise SDL (%s)\n", SDL_GetError());
 			return;
 		}
 	}
 
-	if (!IMG_Init(IMG_INIT_PNG))
-	{
+	// Initialisation des fonctionnalités liées aux images de la SDL
+	if (!IMG_Init(IMG_INIT_PNG)) {
 		printf("[-] ERROR -  Failed to initialise SDL_Image (%s)\n", SDL_GetError());
 		return;
 	}
 
+	// Initialisation de la fenêtre
 	window = SDL_CreateWindow("Light's out", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN + SDL_WINDOW_RESIZABLE);
-	if (!window)
-	{
+	if (!window) {
 		printf("[-] ERROR -  Failed to create SDL window (%s)\n", SDL_GetError());
 		return;
 	}
 
-	int windowh;
-	int windoww;
+	// Récupération de la largeur et la hauteur de le fenêtre
 	SDL_GetWindowSize(window, &windoww, &windowh);
 
+	// Initilisation du rendeur
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
 	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+
+	// Instanciation des textures, surfaces et rectangles
+	SDL_Texture*** lights;
+	SDL_Surface*** images_lights;
+	SDL_Rect** position_lights;
+
+	SDL_Texture* texture1;
+	SDL_Surface* image;
+	SDL_Rect positionRect1;
+
+	SDL_Texture* texture2;
+	SDL_Surface* rectangle;
+	SDL_Rect positionRect2;
+
+	// Initialisation des textures, surfaces
+	image = IMG_Load("data/fond.jpg");
+	texture1 = SDL_CreateTextureFromSurface(renderer, image);
+	SDL_FreeSurface(image);
+
+	rectangle = SDL_CreateRGBSurface(0, windoww * 1480 / 1920, windowh * 220 / 1080, 32, 0, 0, 0, 0);
+	SDL_FillRect(rectangle, NULL, SDL_MapRGB(rectangle->format, 0, 0, 0));
+	texture2 = SDL_CreateTextureFromSurface(renderer, rectangle);
+	SDL_FreeSurface(rectangle);
+
+	// Initialisation des rectangles
+	positionRect1.x = 0;
+	positionRect1.y = 0;
+
+	while (!quit){
+		SDL_RenderClear(renderer);
+		SDL_RenderCopy(renderer, texture1, NULL, &positionRect1);
+		SDL_RenderPresent(renderer);
+	}
+
+	// Destruction du rendeur, de la fenêtre et de la SDL
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
-*/
--- SDL_local.h
+++ SDL_local.h
@@ -1,5 +1,5 @@
 #include "./librairies/SDL2-2.0.6/include/SDL.h"
-//#include "./librairies/SDL2-2.0.6/include/SDL_image.h"
+#include "./librairies/SDL2-2.0.6/include/SDL_image.h"
 //#include "./librairies/SDL2-2.0.6/include/SDL_ttf.h"
 
 #include <stdio.h>
